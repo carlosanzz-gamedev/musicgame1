96306fc4f6bfd7ceb99c2116fe95572416f86481
Carlosanzz <35037107+Carlosanzz@users.noreply.github.com>
2024-06-26 00:19:33 +0200

    Added first beat sync

---
+++ Assets/Scripts/PulseToTheBeat.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PulseToTheBeat : MonoBehaviour
+{
+    [SerializeField] private bool _useTestBeat;
+    [SerializeField] private float _pulseSize = 1.15f;
+    [SerializeField] private float _returnSpeed = 5f;
+    private Vector3 _startSize;
+
+    private void Start()
+    {
+        _startSize = transform.localScale;
+        if (_useTestBeat)
+        {
+            StartCoroutine(TestBeat());
+        }
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _startSize, Time.deltaTime * _returnSpeed);
+    }
+
+    IEnumerator TestBeat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Pulse();
+        }
+    }
+
+    public void Pulse()
+    {
+        transform.localScale = _startSize * _pulseSize;
+    }
+}
